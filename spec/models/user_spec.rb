8f2d3057891bf7bbb4db166703d78ebb72fbad4d
Dombrich Vladimir <domb_vl@live.ru>
2013-03-20 16:25:39 +1100

    dobavlena auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -94,6 +94,33 @@ describe User do
 			@user.should respond_to(:encrypted_password)
 		end
 
+		it "should set the encrypted password" do
+			@user.encrypted_password.should_not be_blank
+		end
+
+		it "should be true if the passwords match" do
+			@user.has_password?(@attr[:password]).should be_true
+		end
+
+		it "should be false if the passwords don't match" do
+			@user.has_password?("invalid").should be_false
+		end
+
+		it "should return nil on email/password mismatch" do
+			wrong_password_user = User.authenticate(@attr[:email], "password")
+			wrong_password_user.should be_nil
+		end
+
+		it "should return nil for an email address with no user" do
+			nonexistent_user = User.authenticate("bad@domen.ru", @attr[:password])
+			nonexistent_user.should be_nil
+		end
+
+		it "should return the user on email/password match" do
+			mathcing_user = User.authenticate(@attr[:email], @attr[:password])
+			mathcing_user.should == @user
+		end
+
 	end
 
 end
