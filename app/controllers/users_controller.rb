67ef0ba16374300b2ba3f552dad59c62d495f057
Vladimir Dombrich <domb_vl@live.ru>
2013-03-21 16:28:02 +1100

    Dobavlena obrabotka formy registracii

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
 		@user = User.new
 		@title = "Sign up"
   	end
+
+  	def create
+  		@user = User.new(params[:user])
+  		if @user.save
+  		else
+  			@title = "Sign up"
+  			render :new
+  		end
+  	end
 end
