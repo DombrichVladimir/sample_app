055f55ddc737f258405c71f17dd06e0d4229622a
Vladimir Dombrich <domb_vl@live.ru>
2013-03-21 17:16:40 +1100

    Dobavleno uspeshnoe sohranenie formy

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -70,5 +70,27 @@ describe UsersController do
                 response.should render_template('new')
             end
         end
+
+        describe "successful" do
+
+            before(:each) do
+                @attr = { 
+                    :name => "Test User", 
+                    :email => "test@success.com", 
+                    :password => "test123test", 
+                    :password_confirmation => "test123test" }
+            end
+
+            it "should create a user" do
+                lambda do
+                    post :create, :user => @attr
+                end.should change(User, :count).by(1)
+            end
+
+            it "should redirect to the user show page" do
+                post :create, :user => @attr
+                response.should redirect_to(user_path(assigns(:user)))
+            end
+        end
     end
 end
