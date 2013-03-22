dc295f8b2b57d4c4f1885b56cba0a79c496d0900
Vladimir Dombrich <domb_vl@live.ru>
2013-03-22 16:50:27 +1100

    Idu domoy

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  	def new
+  		@title = "Sign in"
+  	end
+
+  	def create
+  		user = User.authenticate(params[:session][:email],
+  								params[:session][:password])
+
+  		if user.nil?
+  			flash.now[:error] = "Invalid email/password combination."
+  			@title = "Sign in"
+  			render 'new'
+  		else
+  			sign_in user
+  			redirect_to user
+  		end
+  	end
+
+  	def destroy
+
+  	end
+
+  	def current_user=(user)
+  		@current_user = user
+  	end
+
+end
